c0221595339bb3211512aeaacd9df266f838f562
Niccolo Naso <you@example.com>
2022-06-14 16:54:20 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
+import data from './articles.json';
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='navbar'>
+          <span className='website-name'>{data.articles[0].source.name}</span>
+          <div className='navbar-buttons'>
+            <button type='button'>Login</button>
+            <button type='button'>Sig in</button>
+          </div>
+          
+        </div>
+        <h1 className='page-title'>{data.articles[0].title}</h1>
+
+        <h2 className='page-subtitle'>{data.articles[0].description}</h2>
+
+        <div className='container-img'>
+          <span className='date-articles'>Data di pubblicazione: {data.articles[0].publishedAt}</span>
+          <img src={data.articles[0].urlToImage} className='img-article'></img>
+        </div>
+        
+        <p className='article-body'>{data.articles[0].content}</p>
+
+        <span className='article-author'>Autore: {data.articles[0].author}</span>
+        {/* <ul>
+            {data.articles.map(article => 
+              <li>{article.title}</li>
+            )}
+        </ul> */}
+
       </header>
+        
+        
     </div>
   );
 }
